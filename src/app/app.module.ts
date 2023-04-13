80063e87f22534bdc90e207e7b5488fca34a0c1e
Safwen Fraj Mrad <frajsafwen.m'rad@esprit.tn>
2023-04-13 02:53:57 +0100

    Exercise 1--5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 import { FormsModule } from '@angular/forms';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OffresEmploiComponentComponent
+    OffresEmploiComponentComponent,
+    ArticlesComponentComponent
   ],
   imports: [
     BrowserModule,
